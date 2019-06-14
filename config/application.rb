2794d643d29858c4ff8336b7d8d30a93bd27cca6
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-06-14 19:28:37 +0900

    初期設定を追加
    - ja.yml を追加
    - デフォルトのローケルとタイムゾーンを日本に設定
    - ジェネレーターの設定を変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module GithubMeishiGenerator
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
