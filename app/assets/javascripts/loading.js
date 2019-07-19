16304d6bc3cab4eafc279fb3eb774523ae913a6a
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-07-19 20:22:53 +0900

    twitterアカウントの入力フォームに「@」を表示

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,12 +1,12 @@
 document.addEventListener('turbolinks:load', function() {
   if (document.getElementById("meishi-form")) {
     let img = document.createElement('img');
-    img.src = "https://media0.giphy.com/media/VBwXWPvUdxzPi/200w_d.gif?cid=5c98ac3e5d2e9e306b7a416e454b6c9f&rid=200w_d.gif"
+    img.src = "https://raw.githubusercontent.com/matt-note/it-benkyokai-meishi-generator/master/app/assets/images/giphy-downsized.gif";
 
     let submit_button = document.querySelector("input[type='submit']");
     submit_button.addEventListener("click", () => {
-      document.getElementById("loading").appendChild(img)
-      document.getElementById("text").innerText = "名刺作成中…"
+      document.getElementById("loading").appendChild(img);
+      document.getElementById("text").innerText = "名刺作成中…";
     })
   }
 })
--- app/validators/user_validator.rb
+++ app/validators/user_validator.rb
@@ -1,17 +1,10 @@
 class UserValidator < ActiveModel::Validator
   def validate(user)
-    require_at_mark_in_twitter_account(user)
     require_github_login(user)
     require_twitter_account(user)
   end
 
   private
-    def require_at_mark_in_twitter_account(user)
-      if user.twitter_account[0] != "@"
-        user.errors.add(:twitter_account, "の先頭は@から始めてください。")
-      end
-    end
-
     def require_github_login(user)
       begin
         open("https://github.com/#{user.login}")
@@ -22,7 +15,7 @@ class UserValidator < ActiveModel::Validator
 
     def require_twitter_account(user)
       begin
-        open("https://twitter.com/#{user.twitter_account_remove_at}")
+        open("https://twitter.com/#{user.twitter_account}")
       rescue OpenURI::HTTPError
         user.errors.add(:twitter_account, "を見つけられませんでした。")
       end
