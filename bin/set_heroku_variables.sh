3bf18db0198440183b493cec4e9f39c7aade7feb
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-07-19 23:14:26 +0900

    Cloud Storage + Heroku の設定を追加

---
+++ bin/set_heroku_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 使い方:
+# bash bin/set_heroku_variables.sh
+heroku config:set RAILS_MASTER_KEY="$(< config/master.key)"
+heroku config:set GOOGLE_APPLICATION_CREDENTIALS="$(< config/secrets/it-benkyoukai-meishi.json)"
