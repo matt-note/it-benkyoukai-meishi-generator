25766343381b15ed243afd6e486700e23a48305c
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-07-29 23:39:31 +0900

    navbarを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "users#new"
   resources :users, only: %i[index show new create]
+
+  get "about", to: "static_pages#about"
+  get "faq", to: "static_pages#faq"
 end
