37adfdbc89e7d1536520730643dce1f76933cc77
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-08-14 01:05:43 +0900

    ダウンロードした画像のpathをインスタンスメソッドで出力するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   validates :login, presence: true
   validates :twitter_account, presence: true
   validates_with UserValidator
+
+  def avatar_path
+    Rails.root.join("tmp/#{login}.jpg").to_s
+  end
+
+  def github_qrcode_path
+    Rails.root.join("tmp/#{login}-github-qrcode.png").to_s
+  end
+
+  def twitter_qrcode_path
+    Rails.root.join("tmp/#{login}-twitter-qrcode.png").to_s
+  end
 end
