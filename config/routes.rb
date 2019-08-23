7da7ce65b008f6383733adb56e715b2ec8dbdaf8
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-08-24 00:16:40 +0900

    プライバシーポリシー・免責事項を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "users#new"
   resources :users, only: %i[index show new create]
   get "about", to: "static_pages#about"
+  get "privacy", to: "static_pages#privacy"
+  get "disclaimer", to: "static_pages#disclaimer"
   match "*path" => "application#routing_error", via: :all
 end
